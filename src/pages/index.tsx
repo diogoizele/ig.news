5d13159f4880ced97fff8e130849a156ae9891ac
Diogo Izele <diogo.izele@cwi.com.br>
2022-07-25 21:16:51 -0300

    feat: creates static site generation api request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log(product);
+
   return (
     <>
       <Head>
@@ -17,12 +29,35 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product?.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LPaldCMYViHrzZYI1LcbncW", {
+    expand: ["product"],
+  });
+
+  const timeInDay = 60 * 60 * 24; // 24 hours
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: timeInDay, // Time in seconds
+  };
+};
