3af01bbac48af15c022cdc20afc22eb702dec8bd
Diogo Izele <diogo.izele@cwi.com.br>
2022-07-25 22:51:17 -0300

    feat: implements login and logout function in sigin button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,36 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const handleSignIn = () => {
+    signIn("github");
+  };
+
+  const handleSignOut = () => {
+    signOut();
+  };
+
+  return session ? (
+    <button
+      onClick={handleSignOut}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#04d361" />
-      Diogo Izele
+      {session?.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={handleSignIn}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
